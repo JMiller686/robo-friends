edd7f79fe8ffcaedec71f77813afaac22699188c
JMiller686 <jmiller686@gmail.com>
2019-02-22 14:16:39 -0600

    added hello component

---
+++ src/Hello.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './Hello.css'
+
+class Hello extends Component {
+	render(){
+		return(
+			<div>
+				<h1>Hello World!</h1>
+				<p>Welcome to React</p>
+			</div>
+		);
+	}
+}
