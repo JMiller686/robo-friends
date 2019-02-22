39c3eeb037d32ab496df5286f69f0e205366fe82
JMiller686 <jmiller686@gmail.com>
2019-02-22 15:31:29 -0600

    added robot info into robots.js

--- src/Card.js
+++ src/Card.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const Card = () => {
+const Card = ({ name, email, id }) => {
 	return(
 		<div className="bg-light-blue dib br3 pa3 ma2 grow bw2 shadow-5">
-			<img src="https://robohash.org/test?200x200" alt="bar bo bot" />
+			<img src={`https://robohash.org/${id}?200x200`} alt="bar bo bot" />
 			<div className="tc">
-				<h2>Jane Doe</h2>
-				<p>jane.doe@gmail.com</p>
+				<h2>{name}</h2>
+				<p>{email}</p>
 			</div>
 		</div>
 	);
