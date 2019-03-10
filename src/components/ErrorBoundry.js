9588bb705ba2e226f2a19b6fc788e219a5246bae
JMiller686 <jmiller686@gmail.com>
2019-03-10 13:12:46 -0500

    import errorboundry to app.js

--- src/components/ErrorBoundry.js
+++ src/components/ErrorBoundry.js
@@ -7,6 +7,19 @@ class ErrorBoundry extends Component {
 			hasError: false
 		}
 	}
+
+	componentDidCatch(err, info) {
+		this.setState({
+			hasError: true
+		})
+	}
+
+	render() {
+		if(this.state.hasError) {
+			return <h1>Ooops. That is not good.</h1>
+		}
+		return this.props.children;
+	}
 }
 
 export default ErrorBoundry;
